f36728220f69617a068ef20ecfd7df984ddfeecd
lucas.barboza <lucas.barboza@coopersystem.com.br>
2025-01-03 17:30:09 -0300

    feature: Tela de cadastro
    Co-authored-by: Ester Lino <esteerlino@gmail.com>

--- front/src/app/components/auth/pages/register/register.component.ts
+++ front/src/app/components/auth/pages/register/register.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { SharedComponents } from '../../../../shared/shared.components';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [SharedComponents],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
   standalone: true,
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  iconLogo = '../../../../../assets/icons/hosthub_logo.png';
+}
