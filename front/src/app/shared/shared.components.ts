399fe991b2c9fd813326d4d78ec184394ee722e9
lucas.barboza <lucas.barboza@coopersystem.com.br>
2025-01-03 17:29:54 -0300

    feature: Criação de componentes compartilhados
    Co-authored-by: Ester Lino <esteerlino@gmail.com>

---
+++ front/src/app/shared/shared.components.ts
@@ -0,0 +1,4 @@
+import { CardComponent } from './components/card/card.component';
+import { LogoNameComponent } from './components/logo-name/logo-name.component';
+
+export const SharedComponents = [CardComponent, LogoNameComponent];
