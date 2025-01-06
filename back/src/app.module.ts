0bda564db46a3a4b3009878a0e97aafe5a829f27
Bertolazi <gabriel.basto.bertolazi@gmail.com>
2025-01-06 14:04:39 -0300

    feat busca com filtros

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { BuscaModule } from './busca/busca.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, BuscaModule],
 })
 export class AppModule {}
