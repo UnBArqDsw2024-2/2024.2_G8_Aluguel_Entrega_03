1097b592ab4f5a8e47fdeff5738f2f1ec7b3efc9
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-01-07 00:34:14 -0300

    fix: fix DTO interfaces for password reset functionality

--- front/src/app/core/services/forgot-password.service.ts
+++ front/src/app/core/services/forgot-password.service.ts
@@ -1,10 +1,26 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {
-  ApiResponse,
-  RequestPasswordResetDto,
-} from '../../interfaces/forgot-password.interface';
 import { catchError, Observable, throwError } from 'rxjs';
+
+export interface RequestPasswordResetDto {
+  email: string;
+}
+
+export interface VerifyCodeDto {
+  email: string;
+  code: string;
+}
+
+export interface ResetPasswordDto {
+  token: string;
+  newPassword: string;
+}
+
+export interface ApiResponse<T = void> {
+  success: boolean;
+  message?: string;
+  data?: T;
+}
 
 @Injectable({
   providedIn: 'root',
