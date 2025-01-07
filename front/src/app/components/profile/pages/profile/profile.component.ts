24a79c32639170b4e2201ce1ffd7341b0cfc38c7
Alexandre Beck <alexandrebeck2003@gmail.com>
2025-01-06 23:38:28 -0300

    adiciona excluir conta

--- front/src/app/components/profile/pages/profile/profile.component.ts
+++ front/src/app/components/profile/pages/profile/profile.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import {
@@ -35,13 +36,19 @@ export class UserAdapter {
 
 @Component({
   selector: 'app-profile',
-  imports: [ReactiveFormsModule, HttpClientModule, SharedComponents],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    SharedComponents,
+  ],
   providers: [ApiService],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
 export class ProfileComponent implements OnInit {
   profileForm!: FormGroup;
+  showDeleteModal = false;
 
   constructor(private fb: FormBuilder, private apiService: ApiService) {}
 
@@ -61,5 +68,26 @@ export class ProfileComponent implements OnInit {
     this.profileForm.valueChanges.subscribe((values) => {
       console.log('Mudanças no formulário detectadas:', values);
     });
+  }
+
+  openDeleteModal() {
+    this.showDeleteModal = true;
+  }
+
+  closeDeleteModal() {
+    this.showDeleteModal = false;
+  }
+
+  confirmDeleteAccount() {
+    this.apiService.delete('users', '123456789').subscribe(
+      (response) => {
+        console.log('Conta excluída com sucesso!', response);
+        this.closeDeleteModal();
+      },
+      (error) => {
+        console.error('Erro ao excluir conta:', error);
+        this.closeDeleteModal();
+      }
+    );
   }
 }
